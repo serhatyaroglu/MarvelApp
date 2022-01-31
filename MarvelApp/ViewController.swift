49db093af24b407d413a6cd4dbb0b3f4d9c0879c
Serhat Yaroglu <serhat@elephantapps.co>
2022-01-31 11:49:39 +0300

    Initial Commit

---
+++ MarvelApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarvelApp
+//
+//  Created by serhat yaroglu on 31.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
